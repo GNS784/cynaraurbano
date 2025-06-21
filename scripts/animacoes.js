ff927e80c1868521ba1180876184fe45d3d5c0a7
Gustavo Sampaio <ggnnss784@gmail.com>
2025-06-20 22:49:55 -0300

    Finalização do site

--- scripts/animacoes.js
+++ scripts/animacoes.js
@@ -33,3 +33,21 @@ function scrollDireita() {
 function scrollEsquerda() {
   carrosselServicos.scrollLeft -= larguraItemServicos;
 }
+
+
+let menu = window.document.getElementById("menu")
+function clicarMenu(){
+    menu.style.left = "0px"
+}
+function clicarSair(){
+    menu.style.left = "-100%"
+}
+
+function clicarSair(){
+    menu.style.left = "-100%"
+}
+menu.addEventListener("click", function(event) {
+    if (event.target.tagName === "A") {
+        menu.style.left = "-100%";
+    }
+});
